e1b92594e1d7a3c1b7bc40bb23f1717f677830ba
koradasandhyadevi <119841076+koradasandhyadevi@users.noreply.github.com>
2024-01-08 11:22:24 +0530

    import java.util.*; public class Fun{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String a = sc.nextLine();         System.out.print(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Fun{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.print(a);
+    }
+}
